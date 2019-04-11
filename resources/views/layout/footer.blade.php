45f02c0c4b327e916b387ec1f0af3a8b7aa4b09c
Samuel Loft <sam@devloft.co.uk>
2019-04-11 15:26:51 +0100

    Added social section

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -8,6 +8,21 @@
             <span>Email: info@velway.com | Phone: 0123 456 789 | Fax: 0123 456 789</span>
             <span>Location: Velway, Landsberg, Lichfield Road Ind. Est., Tamworth, Staffordshire B79 7XB.</span>
         </div>
+
+        <div class="social__footer">
+            <a href="">
+                <img src="{{ asset('images/facebook.png') }}" alt="Facebook">
+            </a>
+            <a href="">
+                <img src="{{ asset('images/twitter.png') }}" alt="Twitter">
+            </a>
+            <a href="">
+                <img src="{{ asset('images/linked-in.png') }}" alt="LinkedIn">
+            </a>
+            <a href="">
+                <img src="{{ asset('images/youtube.png') }}" alt="Youtube">
+            </a>
+        </div>
     </div>
 </footer>
 <footer class="copyright">
