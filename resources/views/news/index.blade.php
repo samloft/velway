52c1d43b384658612126b67d4a7e93c06df4affb
Samuel Loft <sam@devloft.co.uk>
2019-04-14 10:16:41 +0100

    Added more content for pages

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,7 +3,26 @@
 @section('page.title', 'News')
 
 @section('content')
-    <div class="container">
-        News
+    <div class="news">
+        <div class="container pt-4 pb-4">
+            <h2 class="pb-3">{{ __('News') }}</h2>
+
+            <div class="card">
+                <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title">Velway raise £1000 for Bliss</h3>
+                    <p class="card-text">
+                        Id velit senserit constituam eum. Ferri deleniti definitionem sea ut, mel ei oratio soluta. At
+                        altera virtute iuvaret nam, eam maiorum invidunt ut, ne has mollis iisque. Pri at mundi soleat
+                        deserunt, cum tota appareat in. Ne duo odio lorem commune, vocibus oporteat cu eum, ad animal
+                        timeam patrioque pri.Delectus ponderum atomorum ne has, ut qui unum neglegentur consequuntur,
+                        erant option argumentum in vel. Est id atomorum periculis instructior, fuisset appetere
+                        complectitur in usu. An movet liberavisse vix. Dicunt tractatos at nam, vis eu movet facete
+                        option, ne pri dolores contentiones. Nibh lobortis adipiscing ex sed, sumo virtute copiosae pri
+                        no, te erat feugait propriae.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -3,7 +3,118 @@
 @section('page.title', 'Services we provide')
 
 @section('content')
-    <div class="container">
-        Services
+    <div class="heading__curved">
+        <div class="container pt-4 pb-4">
+            <h3 class="pb-3">{{ __('Property Management') }}</h3>
+
+            <p>{{ __('Velway are able to deliver planned, preventative maintenance, reactive and emergency works as well as offer an in-house facilities management provision to clients’ requirements nationally.') }}</p>
+        </div>
+    </div>
+
+    <div class="container management__options">
+        <div class="row text-center mt-5">
+            <div class="col-lg-3">
+                <img src="{{ asset('images/reactive_maintenance.png') }}" alt="Reactive Maintenance">
+
+                <h4>{!! __('Reactive<br>Maintenance') !!}</h4>
+
+                <p>
+                    {{ __('Everything from a broken tap to a leaking roof or faulty AC.') }}
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <img src="{{ asset('images/planned_maintenance.png') }}" alt="Planned Maintenance">
+
+                <h4>{!! __('Planned<br>Maintenance') !!}</h4>
+
+                <p>
+                    {{ __('Cyclical and routine maintenance to Increase lifecycle longevity, ensure compliance or maintain warranties.') }}
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <img src="{{ asset('images/risk_assessment.png') }}" alt="Risk Assessment">
+
+                <h4>{!! __('Risk Assessment<br>Management') !!}</h4>
+
+                <p>
+                    {{ __('Completion of all property related risk assessments and the management of all significant findings to make sure you are compliant.') }}
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <img src="{{ asset('images/building_inspection.png') }}" alt="Building Inspection">
+
+                <h4>{!! __('Routine Building<br>Inspection') !!}</h4>
+
+                <p>
+                    {{ __('Velway manage a schedule of routine inspection of tenanted, used or void buildings managing any issues that may arise.') }}
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="off-white">
+        <div class="container compliance">
+            <h3 class="pt-4 pb-3">{{ __('Compliance') }}</h3>
+
+            <div class="row">
+                <div class="col-2">
+                    <img src="{{ asset('images/fire.png') }}" alt="Fire">
+                </div>
+                <div class="col">
+                    <h4>{{ __('Fire') }}</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maorbi accumsan ipsum velit.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2">
+                    <img src="{{ asset('images/water.png') }}" alt="Water">
+                </div>
+                <div class="col">
+                    <h4>{{ __('Water') }}</h4>
+                    <p>
+                        {{ __('We manage all elements of legionella control from the L* risk assessment through to ensuring all significant findings are closed down, meaning all properties comply with the L8 Acop standards') }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2">
+                    <img src="{{ asset('images/asbestos.png') }}" alt="Asbestos">
+                </div>
+                <div class="col">
+                    <h4>{{ __('Asbestos') }}</h4>
+                    <p>
+                        {{ __('Lorem ipsum dolor sit amet, proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maorbi accumsan ipsum velit.') }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2">
+                    <img src="{{ asset('images/health_and_safety.png') }}" alt="Health and Safety">
+                </div>
+                <div class="col">
+                    <h4>{{ __('Health and Safety') }}</h4>
+                    <p>
+                        {{ __('Lorem ipsum dolor sit amet, proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maorbi accumsan ipsum velit.') }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2">
+                    <img src="{{ asset('images/dda.png') }}" alt="DDA">
+                </div>
+                <div class="col">
+                    <h4>{{ __('DDA') }}</h4>
+                    <p>
+                        {{ __('Lorem ipsum dolor sit amet, proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet maorbi accumsan ipsum velit.') }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
