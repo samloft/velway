6ddb5028151079607826440f94846f21bde9c3d8
Samuel Loft <sam@devloft.co.uk>
2019-04-14 09:14:22 +0100

    Added initial styling for contact page

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,6 +4,45 @@
 
 @section('content')
     <div class="container">
-        Contact Us
+        <div class="row">
+            <div class="col-lg-7 pr-5">
+                <h2 class="mb-5 mt-5">{{ __('Contact Us') }}</h2>
+
+                <form action="" class="form-contact pb-5">
+                    <div class="form-group">
+                        <input class="form-control" name="name" placeholder="Full Name:" autocomplete="off">
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" name="email" placeholder="Email Address:" autocomplete="off">
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" name="telephone" placeholder="Telephone:" autocomplete="off">
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" name="company" placeholder="Company Name:" autocomplete="off">
+                    </div>
+
+                    <div class="form-group">
+                        <textarea rows="8" class="form-control" name="message" placeholder="Message:"></textarea>
+                    </div>
+
+                    <div class="btn btn-dark">{{ __('Submit') }}</div>
+                </form>
+            </div>
+
+            <div class="col-lg-5 chat">
+                <h2 class="heading__white mb-5">{{ __('Let\'s Chat?') }}</h2>
+
+                <span>{{ ('Email: Velway@velway.com') }}</span>
+                <span>{{ __('Phone: 0123 456 789') }}</span>
+                <span>{{ __('Fax: 0123 456 789') }}</span>
+                <span>{{ __('Location: Velway, Landsberg,') }}</span>
+                <span>{{ __('Lichfield Road Ind. Est., Tamworth,') }}</span>
+                <span>{{ __('Staffordshire B79 7XB.') }}</span>
+            </div>
+        </div>
     </div>
 @endsection
