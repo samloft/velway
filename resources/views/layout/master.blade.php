863e76ee4980b53c74909824cea15c83a00a941c
Samuel Loft <sam@devloft.co.uk>
2019-04-23 18:37:07 +0100

    Added animation popout for contact icon

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ env('APP_NAME') }} - @yield('page.title')</title>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/aos.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
 
@@ -16,6 +18,15 @@
 
 <div class="contact--floated">
     <i class="fas fa-comment-dots"></i>
+</div>
+
+<div class="contact--floated--expanded">
+    <a href="TEL:{{ $company_details->telephone }}">
+        <i class="fas fa-phone"></i>
+    </a>
+    <a href="MAILTO:{{ $company_details->email }}">
+        <i class="far fa-envelope"></i>
+    </a>
 </div>
 
 @yield('content')
