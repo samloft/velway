1b08a6252f155fb7f4043dda1033d88d9769b30e
Samuel Loft <sam@devloft.co.uk>
2019-04-24 18:38:12 +0100

    Cleaned up contact icon transition to popout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 require('./bootstrap');
 
 $('.contact--floated').on('mouseover', function () {
-    $(this).hide();
+    $(this).hide('slide', {
+        direction: 'left',
+        distance: '20px'
+    }, 300);
+
     $('.contact--floated--expanded').show('slide', {
         direction: 'right'
-    });
+    }, 300);
 });
 
 $('.contact--floated--expanded').on('mouseleave', function () {
