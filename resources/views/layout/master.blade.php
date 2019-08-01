7604e30ce97e12b5d3b2259741f8da5a13c96023
Samuel Loft <sam@devloft.co.uk>
2019-08-01 11:38:20 +0100

    Added favicon

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,10 +12,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143509139-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'UA-143509139-1');
