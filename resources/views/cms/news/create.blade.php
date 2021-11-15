4329ee3a660dd12728a5162d70d12d4243696264
Samuel Loft <sam@devloft.co.uk>
2021-11-15 12:13:31 +0000

    Updated tinymce to version 5

--- resources/views/cms/news/create.blade.php
+++ resources/views/cms/news/create.blade.php
@@ -44,7 +44,7 @@
 @endsection
 
 @section('scripts')
-    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
     <script>
         $('input[name="image"]').on('change', function (e) {
